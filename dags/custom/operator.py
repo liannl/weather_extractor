759f1fbda741120193a084d379b568a780633763
Viktoria Nikolaeva <nv020814@gmail.com>
2024-11-26 10:58:20 +0300

    Merge dev with master

--- dags/custom/operator.py
+++ dags/custom/operator.py
@@ -9,24 +9,23 @@ from custom.hook import WeatherAPIHook
 class WeatherToCSVOperator(BaseOperator):
     @apply_defaults
     def __init__(
-            self, conn_id, city, file_path, **kwargs,
+            self, conn_id, op_kwargs, **kwargs,
     ):
         super(WeatherToCSVOperator , self).__init__(**kwargs)
 
         self._conn_id = conn_id
-        self._city = city
-        self._file_path = file_path
+        self._op_kwargs = op_kwargs
 
     def execute(self, context):
         hook = WeatherAPIHook(self._conn_id)
 
-        self.log.info( f"Fetching weather in {self._city}")
-        result = hook.get_weather_data(city=self._city)
+        self.log.info( f"Fetching weather in {self._op_kwargs['city']}")
+        result = hook.get_weather_data(city=self._op_kwargs['city'])
 
         self.log.info(f"Create DataFrame")
         df = pd.DataFrame([result])
 
-        self.log.info(f"Writing weather to {self._file_path}")
-        df.to_csv(self._file_path, index=False)
+        self.log.info(f"Writing weather to {self._op_kwargs['file_path']}")
+        df.to_csv(self._op_kwargs['file_path'], index=False)
 
-        self.log.info(f"Weather data written to {self._file_path}")
+        self.log.info(f"Weather data written to {self._op_kwargs['file_path']}")
--- dags/extract_weather.py
+++ dags/extract_weather.py
@@ -1,7 +1,6 @@
-from airflow.decorators import dag
 from datetime import datetime, timedelta
 from custom.operator import WeatherToCSVOperator
-from airflow.utils.task_group import TaskGroup
+from airflow.decorators import dag
 
 
 default_args = {
@@ -10,7 +9,6 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
-city = ["Moscow", "Paris", "Berlin"]
 
 @dag(
     default_args=default_args,
@@ -18,24 +16,16 @@ city = ["Moscow", "Paris", "Berlin"]
     schedule_interval=timedelta(days=1),
     catchup=False
 )
-def extract_weather_dag_for():
+def extract_weather_dag():
+    WeatherToCSVOperator.partial(
+        task_id="extract_data",
+        conn_id="weather_api"
+    ).expand(
+        op_kwargs=[
+            {"city": "Moscow", "file_path": "/opt/airflow/data/Moscow_20241126.csv"},
+            {"city": "Paris", "file_path": "/opt/airflow/data/Paris_20241126.csv"},
+            {"city": "Berlin", "file_path": "/opt/airflow/data/Berlin_20241126.csv"},
+        ]
+    )
 
-    with TaskGroup('tasks_for') as tasks_for:
-        for i in range(city.__len__()):
-            WeatherToCSVOperator(
-                task_id=f"extract_data_from_{city[i]}",
-                conn_id="weather_api",
-                city=city[i],
-                file_path=f"/opt/airflow/data/{city[i]}_20241126.csv",
-            )
-
-    tasks_for
-
-dag = extract_weather_dag_for()
-
-# city = ["Moscow", "Paris", "Berlin"],
-# file_path = [
-#     "/opt/airflow/data/Moscow_20241126.csv",
-#     "/opt/airflow/data/Paris_20241126.csv",
-#     "/opt/airflow/data/Berlin_20241126.csv",
-# ]
+dag = extract_weather_dag()
